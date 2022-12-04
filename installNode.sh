26a1adfc55124cccbe30624667ddd2ca07b4ba15
troycampano <troygeek@gmail.com>
2022-12-04 08:26:17 -0500

    Initial commit of installNode.sh

---
+++ installNode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d /home/$USER/Downloads ]; then
+  mkdir -p /home/echo/Downloads;
+fi
+if [ ! -d /home/$USER/Apps ]; then
+  mkdir -p /home/$USER/Apps;
+fi
+wget -P /home/$USER/Downloads/ https://nodejs.org/dist/v18.12.1/node-v18.12.1-linux-x64.tar.xz
+tar -xf /home/$USER/Downloads/node-v18.12.1-linux-x64.tar.xz -C /home/$USER/Apps
+ln -s /home/$USER/Apps/node-v18.12.1-linux-x64 /home/$USER/Apps/node
+
+echo -e "\nexport NODE_HOME=/home/$USER/Apps/node" >> $HOME/.bashrc
+source $HOME/.bashrc
+echo -e "\nexport PATH=\$NODE_HOME/bin:\$PATH" >> $HOME/.bashrc
+source $HOME/.bashrc
